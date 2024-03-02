4cf389b021ec7f2b3d3d11d700c0ee77860fcba9
Benjamin Weder <benjamin.weder@iaas.uni-stuttgart.de>
2024-03-02 13:04:04 +0100

    Add flask cors dependency

--- app/__init__.py
+++ app/__init__.py
@@ -18,6 +18,7 @@
 # ******************************************************************************
 
 from flask import Flask
+from flask_cors import CORS
 from app.config import Config
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
@@ -27,6 +28,7 @@ from app import Config
 import logging
 
 app = Flask(__name__)
+CORS(app)
 app.config.from_object(Config)
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
